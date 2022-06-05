c614f7390f22dd4591329459901741f67435c578
EmiCon97 <derleuchtturm97@gmail.com>
2022-06-05 19:48:26 -0300

    notoken

--- src/libs/verifyToken.ts
+++ src/libs/verifyToken.ts
@@ -1,8 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
 export const TokenValidation = (req: Request, res: Response, next: NextFunction)=>{
-    console.log('HOLAAAAAAAA')
-    const token = req.header('auth-token');
-    if(!token) return res.status(401).json('Access denied');
+    // const token = req.header('auth-token');
+    // if(!token) return res.status(401).json('Access denied');
     next();
 }
