df69cb3770606a34cb3d1112031df20059b292d8
AngelVega0816 <angelvegaxdpro@hotmail.com>
2022-06-04 23:49:59 -0300

    c

--- index.ts
+++ index.ts
@@ -2,11 +2,14 @@ import app from './src/app';
 import dbConnection  from './src/db/index';
 import dotenv from 'dotenv'
 dotenv.config()
+const port = process.env.PORT || 3000;
 
 async function main(){
     await dbConnection()
-    app.listen(app.get('port'), () => {
-        console.log(`server on port`, app.get('port'))
+    // app.listen(app.get('port'), () => {
+    app.listen(port, () => {
+        console.log(`server on port ${port}`)
+        // console.log(`server on port`, app.get('port'))
     })
 }
 
