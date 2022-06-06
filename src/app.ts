5d8b3b9a75db285ff40c6a94550b54780111c326
EmiCon97 <derleuchtturm97@gmail.com>
2022-06-06 19:02:30 -0300

    commit

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,8 @@ app.use(morgan('dev'));
 // app.use(cors('*'));
 app.use((req, res, next) => {
    res.header('Access-Control-Allow-Origin', CORS_URL); // update to match the domain you will make the request from
-  //  res.header('Access-Control-Allow-Origin', 'https://security-app-puce.vercel.app/'); // update to match the domain you will make the request from
    res.header('Access-Control-Allow-Credentials', 'true');
-   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, auth-token');
    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
    next();
  });
