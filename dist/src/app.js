38cac866d5d0b251a522d05285e123cea451e18e
EmiCon97 <derleuchtturm97@gmail.com>
2022-06-06 15:43:52 -0300

    cors

--- dist/src/app.js
+++ dist/src/app.js
@@ -12,7 +12,7 @@ const app = (0, express_1.default)();
 app.use((0, morgan_1.default)('dev'));
 // app.use(cors('*'));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', CORS_URL); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
