cb9f781e8001430c3d9fb61df360bcaa9352f67b
AngelVega0816 <angelvegaxdpro@hotmail.com>
2022-06-06 14:04:30 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
-import cors from 'cors';
+// import cors from 'cors';
+const { CORS_URL } = process.env
 const routes = require('./routes/index');
 
 const app = express();
@@ -8,15 +9,15 @@ const app = express();
 app.use(morgan('dev'));
 // app.use(cors('*'));
 app.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+   res.header('Access-Control-Allow-Origin', CORS_URL); // update to match the domain you will make the request from
    res.header('Access-Control-Allow-Credentials', 'true');
    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
    next();
  });
-app.use((req, res, next) => {
-  next()
-}, cors({ maxAge: 84600 }))
+// app.use((req, res, next) => {
+//   next()
+// }, cors({ maxAge: 84600 }))
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
