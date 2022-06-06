643dcd227ea20f7abb217e5bc35fc58e5604e378
EmiCon97 <derleuchtturm97@gmail.com>
2022-06-06 20:33:31 -0300

    working commit

--- src/app.ts
+++ src/app.ts
@@ -6,21 +6,7 @@ const routes = require('./routes/index');
 
 const app = express();
 
-
 app.use(morgan('dev'));
-<<<<<<< HEAD
-// app.use(cors());
-// app.use((req, res, next) => {
-//    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-//    res.header('Access-Control-Allow-Credentials', 'true');
-//    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//    next();
-//  });
-app.use((req, res, next) => {
-  next()
-}, cors({ maxAge: 84600 }))
-=======
 // app.use(cors('*'));
 app.use((req, res, next) => {
    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
@@ -32,7 +18,6 @@ app.use((req, res, next) => {
 // app.use((req, res, next) => {
 //   next()
 // }, cors({ maxAge: 84600 }))
->>>>>>> 6de0ec7cde86f6d1458476078927db6455672044
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
