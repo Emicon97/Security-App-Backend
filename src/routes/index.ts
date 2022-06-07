02349a41d5064a1d64b2712cb464cab2bce4f4b6
Enzo <enz997.ing.ind@gmail.com>
2022-06-06 21:04:46 -0300

    logout

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,16 +4,16 @@ import loginRoutes from './logIn';
 import userRoutes from './user';
 import todosRoutes from './toDos'
 import reportRoutes from './report';
-import paginated from './paginated';
-import logout from './logout'
+import paginatedRoutes from './paginated';
+import logoutRoutes from './logout'
 
 const router = Router();
 
-router.use('/logout', logout)
+router.use('/logout', logoutRoutes)
 router.use('/login', loginRoutes);
 router.use('/user', userRoutes);
 router.use('/todos', todosRoutes);
 router.use('/report', reportRoutes);
-router.use('/paginated', paginated)
+router.use('/paginated', paginatedRoutes)
 
 export = router;
