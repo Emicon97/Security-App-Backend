d523567bf329abc0f341f24a29017b8acd81519d
EmiCon97 <derleuchtturm97@gmail.com>
2022-06-12 16:42:59 -0300

    database

--- dist/src/config/config.js
+++ dist/src/config/config.js
@@ -6,9 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config({ override: true });
 exports.default = {
+    MONGO_DATABASE: process.env.MONGO_DATABASE || 'admin',
     MONGO_USER: process.env.MONGO_USER || 'admin',
     MONGO_PASSWORD: process.env.MONGO_PASSWORD || 'admin',
     MONGO_HOST: process.env.MONGO_HOST || 'localhost',
-    MONGO_CONFIG: process.env.MONGO_CONFIG || 'security-app',
+    MONGO_CONFIGURATION: process.env.MONGO_CONFIGURATION || 'security-app',
     PORT: process.env.PORT || 3001
 };
